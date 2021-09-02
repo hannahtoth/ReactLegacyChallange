e11b88a6b8cffecac67ea35ecdb8832c15fe28cb
Hannah <hannahtoth@Hannahs-MacBook-Pro-2.local>
2021-09-02 17:30:40 -0400

    debugged To-Do

--- src/Components/ToDo/ToDoIndex.js
+++ src/Components/ToDo/ToDoIndex.js
@@ -6,32 +6,32 @@ class ToDoList extends Component {
       this.state = {
        list: []
      }
-     this.addItem = this.addItem.bind(this);
-     this.removeItem = this.removeItem.bind(this);
+     this.addTask = this.addTask.bind(this);
+     this.removeTask = this.removeTask.bind(this);
    }
-   addItem(e) {
+   addTask(e) {
     e.preventDefault();
 
     let list = this.state.list;
-    const newItem = document.getElementById("addInput");
-    const form = document.getElementById("addItemForm");
+    const newTask = document.getElementById("addInput");
+    const form = document.getElementById("addTaskForm");
 
-    if (newItem.value != "") {
-      list.push(newItem.value);
+    if (newTask.value != "") {
+      list.push(newTask.value);
       this.setState({
         list: list
       });
-      newItem.classList.remove("is-danger");
+      newTask.classList.remove("is-danger");
       form.reset();
     } else {
-      newItem.classList.add("is-danger");
+      newTask.classList.add("is-danger");
     }
   }
 
-  removeItem(item) {
+  removeTask(task) {
     const list = this.state.list.slice();
     list.some((el, i) => {
-      if (el === item) {
+      if (el === task) {
         list.splice(i, 1);
         return true;
       }
@@ -47,22 +47,22 @@ class ToDoList extends Component {
             <div className="container">
               <section className="section">
               <ul>
-                {this.state.list.map(item => (
-                <li key={item}>{item} &nbsp;
-                  <button className="delete" onClick={() => this.removeItem(item)}>Finished!</button>
+                {this.state.list.map(Task => (
+                <li key={Task}>{Task} &nbsp;
+                  <button className="delete" onClick={() => this.removeTask(Task)}>Finished!</button>
                 </li>))}
               </ul>
               </section>
               <hr />
               <section className="section">
-                <form className="form" id="addItemForm">
+                <form className="form" id="addTaskForm">
                   <input
                     type="text"
                     className="input"
                     id="addInput"
                     placeholder="To-Do List"
                   />
-                  <button className="button is-info" onClick={this.addItem}>
+                  <button className="button is-info" onClick={this.addTask}>
                 Add Task
                 </button>
                 </form>
